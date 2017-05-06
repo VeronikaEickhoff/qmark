d55b3fbed19d39df62b369293d94ead6331ba96c
Aleksei Semin <alllex.semin@gmail.com>
2017-05-06 17:23:09 +0200

    Add headlines for different years

--- fetch_headlines.py
+++ fetch_headlines.py
@@ -11,7 +11,10 @@ output_name = "headlines.txt"
 with open(output_name, 'w') as output:
 	for year in years:
 		for month in months:
-			page = requests.get('https://en.wikipedia.org/wiki/Portal:Current_events/April_2016')
+			url = 'https://en.wikipedia.org/wiki/Portal:Current_events/' + month + '_' + str(year)
+			print "Processing", url
+			page = requests.get(url)
+
 			tree = html.fromstring(page.content)
 
 			items = tree.xpath('//td[@class="description"]/ul/li')
@@ -21,21 +24,3 @@ with open(output_name, 'w') as output:
 					output.write(s)
 					output.write('\n')
 
-
-
-# from bs4 import BeautifulSoup
-
-# data = ""
-# with open('/tmp/wiki.html', 'r') as myfile:
-#     data=myfile.read().replace('\n', '')
-
-# soup = BeautifulSoup(data)
-
-# inner_ul = soup.find('ul', class_='innerUl')
-# inner_items = [li.text.strip() for li in inner_ul.ul.find_all('li')]
-
-# outer_ul_text = soup.ul.span.text.strip()
-# inner_ul_text = inner_ul.span.text.strip()
-
-# result = {outer_ul_text: {inner_ul_text: inner_items}}
-# print result
