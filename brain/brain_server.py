a8383eaa19ce25bb3099c7de3e5d303d8cd4c32c
Aleksei Semin <alllex.semin@gmail.com>
2017-05-07 01:37:04 +0200

    Remove pre-training from headlines

--- brain/brain_server.py
+++ brain/brain_server.py
@@ -2,19 +2,20 @@ from flask import Flask, request
 import brain
 import json
 from pprint import pprint
+
 app = Flask(__name__)
 
 print "Creating the Redis client..."
 redis_client = brain.createRedisClient()
 
-print "Loading predefined headlines..."
-headlines_filename = 'headlines.txt'
-with open(headlines_filename) as f:
-    questions = [x.strip() for x in f.readlines()]
-    ids = map(lambda x: str(x), range(0, len(questions)))
-    brain.recompute_model(ids, questions, redis_client)
+# print "Loading predefined headlines..."
+# headlines_filename = 'headlines.txt'
+# with open(headlines_filename) as f:
+#     questions = [x.strip() for x in f.readlines()]
+#     ids = map(lambda x: str(x), range(0, len(questions)))
+#     brain.recompute_model(ids, questions, redis_client)
 
-print "Computed a model from headlines"
+# print "Computed a model from headlines"
 
 @app.route("/similar/", methods=['GET'])
 def similar():
@@ -34,4 +35,4 @@ def update():
     return "" # an empty response
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
